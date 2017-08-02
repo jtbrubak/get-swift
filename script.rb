15475bbefbba43246154302152c8cdb16d9406d1
Jeff Brubaker <jtbrubak1@gmail.com>
2017-08-02 01:19:23 -0400

    Convert to API

--- script.rb
+++ script.rb
@@ -2,52 +2,65 @@ require 'net/http'
 require 'json'
 require 'haversine'
 
-hub_location = [-37.816664, 144.963848]
-drones = []
-packages = []
-assigned_jobs = { 'assignments' => [], 'unassignedPackageIds' => [] }
-
-JSON.parse(Net::HTTP.get(URI('https://codetest.kube.getswift.co/drones'))).each do |drone|
-  distance_to_return = 0
-  if drone['packages'].empty?
-    distance_to_return += Haversine.distance(hub_location,
-     [drone['location']['latitude'], drone['location']['longitude']]).to_km
-  else
-    distance_to_return += Haversine.distance([drone['location']['latitude'], drone['location']['longitude']],
-     [drone['packages'][0]['destination']['latitude'], drone['packages'][0]['destination']['longitude']]).to_km
-    distance_to_return += Haversine.distance(hub_location,
-     [drone['packages'][0]['destination']['latitude'], drone['packages'][0]['destination']['longitude']]).to_km
-  end
-  drone['time_to_return'] = (3600 * distance_to_return) / 20
-  drones.push(drone)
+def index
+  drones = populate_drones
+  packages = populate_packages
+  drones.sort_by! { |drone| (drone['time_to_return']) }
+  render json: assign_jobs(packages, drones)
+  packages.sort_by! { |package| (package['secs_until_deadline'] - package['time_to_destination']) }
 end
 
-drones.sort_by! { |drone| (drone['time_to_return']) }
+private
+def populate_drones
+  drones = []
+  JSON.parse(Net::HTTP.get(URI('https://codetest.kube.getswift.co/drones'))).each do |drone|
+    distance_to_return = 0
+    drone_location = [drone['location']['latitude'], drone['location']['longitude']]
+    drone_destination = drone['packages'].empty? ? nil :
+      [drone['packages'][0]['destination']['latitude'],
+       drone['packages'][0]['destination']['longitude']]
+    if drone_destination.nil?
+      distance_to_return += distance_from_hub(drone_location)
+    else
+      distance_to_return += (distance_from_hub(drone_destination)
+        + distance_between_points(drone_location, drone_destination)
+    end
+    drone['time_to_return'] = (3600 * distance_to_return) / 20
+    drones.push(drone)
+  end
+  drones
+end
 
-JSON.parse(Net::HTTP.get(URI('https://codetest.kube.getswift.co/packages'))).each do |package|
-  package['secs_until_deadline'] = package['deadline'] - Time.now.to_i
-  distance_to_destination = Haversine.distance(hub_location,
-    [package['destination']['latitude'], package['destination']['longitude']]).to_km
-  package['time_to_destination'] = (3600 * distance_to_destination) / 20
-  if package['time_to_destination'] > package['secs_until_deadline']
-    assigned_jobs['unassignedPackageIds'].push(package['packageId'])
-  else
+def populate_packages
+  packages = []
+  JSON.parse(Net::HTTP.get(URI('https://codetest.kube.getswift.co/packages'))).each do |package|
+    package['secs_until_deadline'] = package['deadline'] - Time.now.to_i
+    distance_to_destination = distance_from_hub(
+      [package['destination']['latitude'], package['destination']['longitude']])
+    package['time_to_destination'] = (3600 * distance_to_destination) / 20
     packages.push(package)
   end
+  packages
 end
 
-packages.sort_by! { |package| (package['secs_until_deadline'] - package['time_to_destination']) }
-puts drones
-puts packages
-
-until packages.empty?
-  if packages.first['time_to_destination'] + drones.first['time_to_return'] > packages.first['secs_until_deadline']
-    assigned_jobs['unassignedPackageIds'].push(packages.first['packageId'])
-  else
-    assigned_jobs['assignments'].push({ droneId: drones.first['droneId'], packageId: packages.first['packageId']})
-    drones.shift
+def assign_jobs(packages, drones)
+  assigned jobs = { 'assignments' => [], 'unassignedPackageIds' => [] }
+  until packages.empty?
+    if packages.first['time_to_destination'] + drones.first['time_to_return'] > packages.first['secs_until_deadline']
+      assigned_jobs['unassignedPackageIds'].push(packages.first['packageId'])
+    else
+      assigned_jobs['assignments'].push({ droneId: drones.first['droneId'], packageId: packages.first['packageId']})
+      drones.shift
+    end
+    packages.shift
   end
-  packages.shift
+  assigned_jobs
 end
 
-puts assigned_jobs
+def distance_from_hub(start_point)
+  Haversine.distance([-37.816664, 144.963848], start_point).to_km
+end
+
+def distance_between_points(start_point, end_point)
+  Haversine.distance(start_point, end_point).to_km
+end
